fca7e4b45e4f2ea5db8d79922acaf6b4653dd9ef
illumi-410 <sandeepparajuli96@gmail.com>
2022-04-27 12:53:38 +0545

    feat:table

--- src/components/tableContent/TableContent.tsx
+++ src/components/tableContent/TableContent.tsx
@@ -10,11 +10,11 @@ const TableContent = ( {props} : any) => {
         <input type="checkbox" name="" id="" />
         <p className={styles.name}>{props.name}</p>
       </div>
-      <p className={styles.p}>{props.username}</p>
-      <p className={styles.p} >{props.email}</p>
-      <p className={styles.p}>{props.phone}</p>
-      <p className={styles.p}>{props.website}</p>
-      <p className={styles.p}>{props.address.street}</p>
+      <p className={styles.items}>{props.username}</p>
+      <p className={styles.items} >{props.email}</p>
+      <p className={styles.items}>{props.phone}</p>
+      <p className={styles.items}>{props.website}</p>
+      <p className={styles.items}>{props.address.street}</p>
     </div>
   )
 }
--- src/components/tableHeader/TableHeader.tsx
+++ src/components/tableHeader/TableHeader.tsx
@@ -7,13 +7,13 @@ const TableHeader = () => {
     <div className={styles.tableHeader}>
         <div className={styles.nameContainer}>
             <input type="checkbox" name="" id="" />
-            <h2 className={styles.name}>Name</h2>
+            <h3 className={styles.name}>Name</h3>
         </div>
-        <h2>Username</h2>
-        <h2>Email</h2>
-        <h2>Phone</h2>
-        <h2>Website</h2>
-        <h2>Address</h2>
+        <h3 className={styles.hItem}>Username</h3>
+        <h3 className={styles.hItem}>Email</h3>
+        <h3 className={styles.hItem}>Phone</h3>
+        <h3 className={styles.hItem}>Website</h3>
+        <h3 className={styles.hItem}>Address</h3>
     </div>
   )
 }
