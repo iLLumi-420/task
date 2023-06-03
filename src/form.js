33ac55b8c49290e6d56d7cc76e3eeac0f3b0da10
illumi-410 <sandeepparajuli96@gmail.com>
2023-06-03 15:42:20 +0545

    seperate validation function

--- src/form.js
+++ src/form.js
@@ -8,24 +8,30 @@ function Form({ schema }) {
     setFormValues((previousData) => ({ ...previousData, [name]: value }));
   };
 
+  const handleValidation = (data, value) => {
+    const { validate } = data;
+    let error = "";
+    if (validate) {
+      const { type, min_length, min_value } = validate;
+      if (type === "string" && value.length < min_length) {
+        error = `Minium length of name should be ${min_length}`;
+      } else if (type === "number" && Number(value) < min_value) {
+        error = `Minium age should be ${min_value}`;
+      }
+    }
+    return error;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const errors = {};
     schema.forEach((data) => {
-      const { name, validate } = data;
+      const { name } = data;
       const value = formValues[name];
-      let error = "";
+      const error = handleValidation(data, value);
 
-      if (validate) {
-        const { type, min_length, min_value } = validate;
-        if (type === "string" && value.length < min_length) {
-          error = `Minium length of name should be ${min_length}`;
-        } else if (type === "number" && Number(value) < min_value) {
-          error = `Minium age should be ${min_value}`;
-        }
-        if (error) {
-          errors[name] = error;
-        }
+      if (error) {
+        errors[name] = error;
       }
     });
     if (Object.keys(errors).length === 0) {
